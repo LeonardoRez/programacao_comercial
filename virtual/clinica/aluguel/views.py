a1efcc2eec7f0db2123b426e68e23acdfabda28e
FeBarbosa <felipe.barbosa@mail.uft.edu.br>
2016-10-04 22:44:33 -0300

    Views - agenda semanal e mensal

--- virtual/clinica/aluguel/views.py
+++ virtual/clinica/aluguel/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Esta e a pagina index de aluguel")
+
+def agendaMes(request, mes_id):
+    return HttpResponse("Esta e a agenda do mes %s." % mes_id)
+
+def agendaSemana(request, semana_id, mes_id):
+    return HttpResponse("Esta e a agenda da semana %s do mes %s." % semana_id % mes_id)

371bc51665dac323d709e97051eb0bf6c5d0a62c
LeonardoRez <leonardorec1@gmail.com>
2016-10-28 15:04:12 -0300

    removendarquivos desnecessarios

--- virtual/clinica/aluguel/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<mes_id>[1-12]+)/$', views.agendaMes, name='agendaMes'),
-    url(r'^(?P<semana_id>[0-5]+)/(?P<mes_id>[1-12]+)/$', views.agendaSemana, name='agendaSemna'),
-]

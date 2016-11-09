3ca213c2d448f34a8d764114adac74cfe9b73ed2
LeonardoRez <leonardorec1@gmail.com>
2016-11-08 22:24:20 -0300

    Autenticando usuario no BD (nao funcionando)

--- virtual/clinica/autenticacao/urls.py
+++ virtual/clinica/autenticacao/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views #views no mesmo diretorio
 
 urlpatterns = [
-    url(r'^login', views.login, name='login'), # chama a tela de login quando a url eh /login
+    url(r'^login', views.Autenticacao.as_view(), name='login'), # chama a tela de login quando a url eh /login
     url(r'^$', views.index, name='index'), # chama o index quando a url eh /
 
 ]
--- virtual/clinica/autenticacao/views.py
+++ virtual/clinica/autenticacao/views.py
@@ -1,11 +1,33 @@
-from django.shortcuts import render
-from django.template import loader
-from django.http import HttpResponse
+from django.views.generic import View
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
-def login(request):
-    # return HttpResponse("Tela de login")
-    # template = loader.get_template('autenticacao/index.html')
-    return render(request, 'autenticacao/login.html', {})
+
+class Autenticacao(View):
+    def get(self,request):
+        return render(request,'autenticacao/login.html', {})
+
+    def post(self,request):
+        resposta={
+            'sucesso': False,
+            'mensagem': ''
+        }
+        usuario = request.POST.get("login")
+        senha = request.POST.get("senha")
+        user = authenticate(username=usuario, password=senha)
+        if user:
+            login(request, user)
+            return redirect('/index')
+        else:
+            resposta['mensagem'] = 'Login ou Senha incorreto(s)'
+
+        return render(request, 'autenticacao/login.html', resposta)
+
+# def login(request):
+#     # return HttpResponse("Tela de login")
+#     # template = loader.get_template('autenticacao/index.html')
+#     return render(request, 'autenticacao/login.html', {})
 
 def index(request):
     return render(request,'index.html',{})

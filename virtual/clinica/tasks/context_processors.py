f4c797ec849f0288d4354ab9e922728c324228ef
LeonardoRez <leonardorec1@gmail.com>
2016-12-02 11:53:08 -0300

    menu 'tarefas' adicionado | dropList superior implementado

--- virtual/clinica/tasks/context_processors.py
+++ virtual/clinica/tasks/context_processors.py
@@ -4,5 +4,12 @@ from .models import Tarefa
 
 def ultimas_tasks(request):
     #ultimas_tarefas = Tarefa.objects.filter().order_by('grau_importancia', 'id')[:5]
-    ultimas_tarefas = Tarefa.objects.filter().order_by('grau_importancia','id')[:5]
-    return {'ultimas_tarefas' : ultimas_tarefas}
+    contexto = {}
+    contexto['ultimas_tarefas'] = Tarefa.objects.filter(done = False).order_by('grau_importancia','id')[:5]
+    contexto['quant_total'] = len(Tarefa.objects.filter(done = False))
+    contexto['quant_urgente'] = len(Tarefa.objects.filter(grau_importancia = 1,done = False))
+    contexto['quant_importante'] = len(Tarefa.objects.filter(grau_importancia = 2,done = False))
+    contexto['quant_simples'] = len(Tarefa.objects.filter(grau_importancia = 3,done = False))
+    contexto['quant_lembrete'] = len(Tarefa.objects.filter(grau_importancia = 4,done = False))
+
+    return contexto
